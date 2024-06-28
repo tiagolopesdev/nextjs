0afb88df32e3e15cd26b6f65f011cfdcdb8eec02
Ricardo Simões <ricardocunha@MacBook-Pro-de-Ricardo.local>
2024-06-28 17:11:05 -0300

    removi o counter da home

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -1,10 +1,8 @@
-import Counter from './counter'
 
 export default function Page() {
     return (
         <div>
-            <h1>Hello, Next.js!</h1>
-            <Counter />
+            <h1>Hello, Next.js!</h1>         
         </div>
         
     )
