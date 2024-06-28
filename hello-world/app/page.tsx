38163829141771eb6b4e3d320eb459fdb9ea4dae
Ricardo Simões <ricardocunha@MacBook-Pro-de-Ricardo.local>
2024-06-28 17:02:42 -0300

    criando a v1 do hello world

---
+++ hello-world/app/page.tsx
@@ -0,0 +1,12 @@
+import Counter from './counter'
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Hello, Next.js!</h1>
+            <Counter />
+        </div>
+        
+    )
+}
+
